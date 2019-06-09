59bd799eaf83c23855a9ccb6e74636686a6dcf05
Mateus Calza <mateushenrique17@gmail.com>
2019-06-08 21:07:57 -0300

    API do job.

--- src/utils/job.js
+++ src/utils/job.js
@@ -1,24 +1,48 @@
-import { READY } from './states';
+import { READY, RUNNING, BLOCKED } from './states';
 
 export default class Job {
   constructor({ name, arrivalTime, burstTime }) {
     this.name = name;
     this.arrivalTime = arrivalTime;
     this.burstTime = burstTime;
+    this.remainingTime = burstTime;
 
     this.history = Array(arrivalTime);
     this.currentState = READY;
+    this.ended = false;
 
     this.tick(arrivalTime);
   }
 
   tick(time) {
+    if (this.ended) {
+      return this;
+    }
+
+    if (this.currentState === RUNNING) {
+      this.remainingTime -= 1;
+      if (this.remainingTime <= 0) {
+        this.end();
+      }
+    }
+
     this.history[time] = this.currentState;
+    return this;
+  }
+
+  dispatch() {
+    this.currentState = RUNNING;
+    return this;
+  }
+
+  block() {
+    this.currentState = BLOCKED;
     return this;
   }
 
   end() {
     this.currentState = null;
+    this.ended = true;
     return this;
   }
 }
